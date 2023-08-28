e389be47a1ec2942c6903337440737ecfe066a9d
Corbin Whitton <undyingsoul.mc@gmail.com>
2023-08-28 11:10:40 -0600

    Upgraded logging.

--- utils/Logger.py
+++ utils/Logger.py
@@ -1,6 +1,6 @@
+import sys
 from colorama import Fore, Back
 import logging
-import logging.config
 from logging.handlers import TimedRotatingFileHandler
 import os
 import pathlib
@@ -44,29 +44,63 @@ class ColorFormatter(logging.Formatter):
             self._style._fmt = "%(message)s"
         return super().format(record)
 
-
 def combining_filter(record):
     record.location = '%s:%s:%s' % (
         record.module, record.funcName, record.lineno)
     record.locationClickable = '%s:%s' % (record.filename, record.lineno)
     return True
 
-
-def setup_logging(
-    default_config='./config/logging.conf',
-    default_level=Setting.loggingLevel
-):
+def setup_logging(default_level=Setting.loggingLevel):
     """
     Setup logging configuration
     """
+    # triedToCreateFile = False
+    # for i in range(0, 2):
+    #     try:
+    #         if os.path.exists(default_config):
+    #             logging.config.fileConfig(default_config, disable_existing_loggers=False)
+    #         else:
+    #             logging.basicConfig(level=default_level)
+    #     except (FileNotFoundError, OSError) as fnfe:
+    #         try:
+    #             loggingFilePath = pathlib.Path(fnfe.filename).parent
+    #             loggingFilePath.mkdir(parents=True, exist_ok=True)
+    #         except Exception as e:
+    #             pass
+    #         if triedToCreateFile:
+    #             print(
+    #                 f"{Back.WHITE}{Fore.RED}CRITICAL: Cannot find or create the logging directory or file, exiting{Back.RESET}{Fore.RESET}")
+    #             raise SystemExit(1)
+    #         else:
+    #             print(
+    #                 f"{Fore.RED}ERROR: Cannot find or create the logging directory or file {loggingFilePath}, creating one..{Fore.RESET}")
+    #             triedToCreateFile = True
+    #             continue
+
+    # logging level of notes should be just higher than info
+    logging.NOTE = 21
+    logging.addLevelName(logging.NOTE, "NOTE")
+
+    # logging level of header should be just higher than info
+    logging.HEADER = 22
+    logging.addLevelName(logging.HEADER, "HEADER")
+
+    # logging level of notes should be just higher than info
+    logging.COMPLETION = 31
+    logging.addLevelName(logging.COMPLETION, "COMPLETION")
+
+    # Create logger
+    logger = logging.getLogger('root')
+    logger.setLevel(logging.DEBUG)
+
+    # Create handlers
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.setLevel(Setting.loggingLevel)
+
     triedToCreateFile = False
     for i in range(0, 2):
         try:
-            if os.path.exists(default_config):
-                logging.config.fileConfig(
-                    default_config, disable_existing_loggers=False)
-            else:
-                logging.basicConfig(level=default_level)
+            file_handler = TimedRotatingFileHandler('logs/latest.log', when="D", interval=1, backupCount=0)
         except (FileNotFoundError, OSError) as fnfe:
             try:
                 loggingFilePath = pathlib.Path(fnfe.filename).parent
@@ -82,29 +116,20 @@ def setup_logging(
                     f"{Fore.RED}ERROR: Cannot find or create the logging directory or file {loggingFilePath}, creating one..{Fore.RESET}")
                 triedToCreateFile = True
                 continue
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.addFilter(combining_filter)
+    file_handler.namer = lambda name: str.replace(name, "latest.log.", "") + ".log"
 
-    # logging level of notes should be just higher than info
-    logging.NOTE = 21
-    logging.addLevelName(logging.NOTE, "NOTE")
+    # Create formatter
+    formatter_timestampedLogWithName = logging.Formatter('[%(asctime)-15s] %(levelname)-10s : %(message)s (%(locationClickable)s)', datefmt='%Y-%m-%d %H:%M:%S')
 
-    # logging level of header should be just higher than info
-    logging.HEADER = 22
-    logging.addLevelName(logging.HEADER, "HEADER")
+    # Set formatter for handlers
+    file_handler.setFormatter(formatter_timestampedLogWithName)
+    stdout_handler.setFormatter(ColorFormatter("%(message)s"))
 
-    # logging level of notes should be just higher than info
-    logging.COMPLETION = 31
-    logging.addLevelName(logging.COMPLETION, "COMPLETION")
+    # Add handlers to logger
+    logger.addHandler(stdout_handler)
+    logger.addHandler(file_handler)
 
-    for h in logging.root.handlers:
-        if type(h) == logging.StreamHandler:
-            h.setLevel(Setting.loggingLevel)
-        if type(h) == TimedRotatingFileHandler:
-            if not Setting.logToFile:
-                logging.root.removeHandler(h)
-                continue
-            h.addFilter(combining_filter)
-            h.namer = lambda name: str.replace(
-                name, "latest.log.", "") + ".log"
-            h.setLevel(logging.DEBUG)
-
+    # Set logging level for external libraries (PIL in this case)
     logging.getLogger("PIL").setLevel(logging.ERROR)
